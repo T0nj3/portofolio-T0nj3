8f6cc4736e8757fe9cd703a4f4876b0197eda4af
T0nj3 <Tkristianseen@gmail.com>
2024-12-07 17:07:11 +0100

    Added project page

--- js/script.js
+++ js/script.js
@@ -1,33 +1,48 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const splashScreen = document.getElementById('splash-screen');
-  const mainContent = document.getElementById('main-content');
-  const countdownElement = document.getElementById('countdown');
-  const themeToggle = document.getElementById('theme-toggle');
+  const hamburgerMenu = document.getElementById('hamburger-menu');
+  const mobileNav = document.querySelector('.mobile-nav');
+  const closeMenuButton = document.getElementById('close-menu');
+  const themeToggleDesktop = document.getElementById('theme-toggle-desktop');
+  const themeToggleMobile = document.getElementById('theme-toggle-mobile');
   const logo = document.getElementById('logo');
   const body = document.body;
-  let countdown = 3;
 
-  const countdownInterval = setInterval(() => {
-    countdown -= 1;
-    countdownElement.textContent = countdown;
+  const updateThemeUI = (theme) => {
+      body.dataset.theme = theme;
+      themeToggleDesktop.textContent = theme === 'light' ? '☀' : '☾';
+      themeToggleMobile.textContent = theme === 'light' ? '☀' : '☾';
+      logo.src = theme === 'light' ? '../assets/darklogo3.png' : '../assets/lightlogo.png';
+      localStorage.setItem('theme', theme);
+  };
 
-    if (countdown === 0) {
-      clearInterval(countdownInterval);
-      splashScreen.style.display = 'none';
-      mainContent.style.display = 'block';
-    }
-  }, 1000);
+  const savedTheme = localStorage.getItem('theme') || 'dark';
+  updateThemeUI(savedTheme);
 
-  themeToggle.addEventListener('click', () => {
-    const currentTheme = body.dataset.theme;
-    if (currentTheme === 'light') {
-      body.dataset.theme = 'dark';
-      logo.src = './assets/lightlogo.png';
-      themeToggle.textContent = '☾';
-    } else {
-      body.dataset.theme = 'light';
-      logo.src = './assets/darklogo3.png';
-      themeToggle.textContent = '☀';
-    }
+  hamburgerMenu.addEventListener('click', () => {
+      mobileNav.classList.add('open');
+  });
+
+  closeMenuButton.addEventListener('click', () => {
+      mobileNav.classList.remove('open');
+  });
+
+  const toggleTheme = () => {
+      const currentTheme = body.dataset.theme;
+      const newTheme = currentTheme === 'light' ? 'dark' : 'light';
+      updateThemeUI(newTheme);
+  };
+
+  themeToggleDesktop.addEventListener('click', toggleTheme);
+  themeToggleMobile.addEventListener('click', toggleTheme);
+});
+
+document.querySelectorAll('.project-card').forEach((card) => {
+  const video = card.querySelector('video');
+  card.addEventListener('mouseenter', () => {
+    video.play();
+  });
+  card.addEventListener('mouseleave', () => {
+    video.pause();
+    video.currentTime = 0;
   });
 });
